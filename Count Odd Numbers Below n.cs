f9aa7e7dda0d5ed9c0d7d9ad2d4b16f29aa48d9a
n-africa <69430340+n-africa@users.noreply.github.com>
2020-08-26 17:33:55 -0400

    Create Count Odd Numbers Below n.cs

---
+++ Count Odd Numbers Below n.cs
@@ -0,0 +1,11 @@
+using System;
+
+public static class Kata
+{
+  public static ulong OddCount(ulong n)
+  {
+    // create variable = n divided by 2
+    var oddNumberCount = n / 2;
+    return oddNumberCount;
+  }
+}
